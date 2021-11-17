630e8539bb26e692d78072def5c2e12f3e808860
Richard Robinson <rrobinson.campos@gmail.com>
2021-11-17 09:53:08 -0300

    test: Size modifiers

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -15,12 +15,23 @@ describe('<Logo />', () => {
   })
 
   it('should render a black label when color is passed', () => {
-    // Renderizar o componente 'render'
     renderWithTheme(<Logo color="black" />)
-    // Selecionar o elemento a ser testado 'screen' - (queries) 'getByTestId'
-    // expect - assertion - asserção - comparação - analise
     expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
       color: '#030517'
+    })
+  })
+
+  it('should render a normal logo when size is default', () => {
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
+
+  it('should render a bigger logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
     })
   })
 })
