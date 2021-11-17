30fb7cceeac6c69e53556c8b1095068d2c2c72c0
Richard Robinson <rrobinson.campos@gmail.com>
2021-11-17 09:42:20 -0300

    test: Logo colors

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,11 +1,26 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Logo from '.'
 
 describe('<Logo />', () => {
   it('should render a white label by default', () => {
     // Renderizar o componente 'render'
+    renderWithTheme(<Logo />)
     // Selecionar o elemento a ser testado 'screen' - (queries) 'getByTestId'
     // expect - assertion - asserção - comparação - analise
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
+  })
+
+  it('should render a black label when color is passed', () => {
+    // Renderizar o componente 'render'
+    renderWithTheme(<Logo color="black" />)
+    // Selecionar o elemento a ser testado 'screen' - (queries) 'getByTestId'
+    // expect - assertion - asserção - comparação - analise
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#030517'
+    })
   })
 })
