2d3b39e87c023c652b49580ac119fc19657163ec
Richard Robinson <rrobinson.campos@gmail.com>
2021-11-17 09:27:54 -0300

    feat: Creating render with theme helper

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -3,11 +3,9 @@ import { render, screen } from '@testing-library/react'
 import Logo from '.'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
-
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render a white label by default', () => {
+    // Renderizar o componente 'render'
+    // Selecionar o elemento a ser testado 'screen' - (queries) 'getByTestId'
+    // expect - assertion - asserção - comparação - analise
   })
 })
